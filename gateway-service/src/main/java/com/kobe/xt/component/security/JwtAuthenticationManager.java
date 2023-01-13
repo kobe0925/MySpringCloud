0a12bb64bc1e1abe84fab94a9e6bdd609e9afc5e
xiangtan <xiangtan@hcogroup.cn>
2023-01-13 17:24:47 +0800

    验证网关可以获取的客户端及用户信息-20230113-1

--- gateway-service/src/main/java/com/kobe/xt/component/security/JwtAuthenticationManager.java
+++ gateway-service/src/main/java/com/kobe/xt/component/security/JwtAuthenticationManager.java
@@ -6,13 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.server.resource.BearerTokenAuthenticationToken;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author kobe_xt
@@ -22,8 +29,8 @@ import reactor.core.publisher.Mono;
  *                  （2）一旦 token 校验通过之后，则将请求交给鉴权管理器（AuthorizationManager）进行鉴权
  * @date 2022/9/14 14:18
  */
-@Component
 @Slf4j
+@Component
 public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
 
     @Autowired
@@ -46,6 +53,15 @@ public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
                     if(oAuth2Authentication == null){
                         return Mono.error(new TokenInvalidException("无效的token！"));
                     }else{
+                        OAuth2Request oAuth2Request = oAuth2Authentication.getOAuth2Request();
+                        String clientId = oAuth2Request.getClientId();
+                        String grantType = oAuth2Request.getGrantType();
+                        Set<String> resourceIds = oAuth2Request.getResourceIds();
+                        Set<String> scope = oAuth2Request.getScope();
+                        Collection<GrantedAuthority> authorities = oAuth2Authentication.getAuthorities();
+                        log.info("当前客户端id:{},授权模式:{},可以访问资源的id列表:{},可以访问资源的scope:{},用户的权限列表为:{}",
+                                clientId,grantType, Arrays.toString(resourceIds.toArray()),
+                                Arrays.toString(scope.toArray()),Arrays.toString(authorities.toArray()));
                         return Mono.just(oAuth2Authentication);
                     }
                 }).cast(Authentication.class);
